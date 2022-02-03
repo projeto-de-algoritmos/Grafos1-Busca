bdfb22de15d03af2d9ead70aeec4fe29fd2e6eaf
FranciscoHeronildo <heronildo.2010@hotmail.com>
2022-02-02 23:16:45 -0300

    Iniciando o problema do Caixeiro Viajante
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

---
+++ src/caixeiroViajante.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define V 4 
+
+int caixeiroViajante(int graph[][V], int s) {
+
+    vector<int> vertice;
+    for (int i = 0; i < V; i++)
+        if(i!=s)
+            vertice.push_back(i);
+   
+}
+
+int main () {
+    
+    return 0;
+}
