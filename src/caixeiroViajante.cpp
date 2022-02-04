1005773d254aada4024f75082d117237eb081377
FranciscoHeronildo <heronildo.2010@hotmail.com>
2022-02-04 16:07:43 -0300

    Criando o ciclo Hamiltoniano
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- src/caixeiroViajante.cpp
+++ src/caixeiroViajante.cpp
@@ -8,7 +8,25 @@ int caixeiroViajante(int graph[][V], int s) {
     for (int i = 0; i < V; i++)
         if(i!=s)
             vertice.push_back(i);
-   
+
+    
+    int min_Rota = INT_MAX;
+    
+    do {
+        
+        int atual_pesoRota = 0;
+        
+        for (int i = 0; i < vertice.size(); i++) {
+            atual_pesoRota += graph[k][vertice[i]];
+            k = vertice[i];
+        }
+        
+        atual_pesoRota += graph[k][s];
+        min_Rota = min(min_Rota, atual_pesoRota);
+
+    } while (next_permutation(vertice.begin(), vertice.end())); 
+    return min_Rota;
+
 }
 
 int main () {
