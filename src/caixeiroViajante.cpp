dc41d8d9da39f07ee01941201a02bcff2d760f74
Nathalia Lorena <nathalialorenacardoso@gmail.com>
2022-02-04 22:56:06 -0300

    Adicionando o grafo
    Co-Authored-By: Francisco Heronildo <heronildo.2010@hotmail.com>

--- src/caixeiroViajante.cpp
+++ src/caixeiroViajante.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define V 4 
+#define V 5 
+
 
 int caixeiroViajante(int graph[][V], int s) {
 
@@ -9,27 +10,36 @@ int caixeiroViajante(int graph[][V], int s) {
         if(i!=s)
             vertice.push_back(i);
 
-    
     int min_Rota = INT_MAX;
-    
     do {
-        
+
         int atual_pesoRota = 0;
-        
+        int k=s;
         for (int i = 0; i < vertice.size(); i++) {
             atual_pesoRota += graph[k][vertice[i]];
             k = vertice[i];
         }
-        
         atual_pesoRota += graph[k][s];
+
         min_Rota = min(min_Rota, atual_pesoRota);
 
-    } while (next_permutation(vertice.begin(), vertice.end())); 
-    return min_Rota;
+    } while (next_permutation(vertice.begin(), vertice.end()));
 
+    return min_Rota;
+   
 }
 
 int main () {
+
+    int graph[][V] = {{ 0, 2, 3, 2, 3 },
+                      { 2, 0, 3, 4, 1 },
+                      { 3, 3, 0, 2, 4 },
+                      { 2, 4, 2, 0, 5 },
+                      { 3, 1, 4, 5, 0 }};
+
+    int s = 0;
+    cout << caixeiroViajante(graph, s);
     
     return 0;
+
 }
