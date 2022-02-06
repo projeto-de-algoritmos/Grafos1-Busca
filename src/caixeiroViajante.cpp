2417bb8d1044860bc80e9e4544074a898a6c0588
Nathalia Lorena <nathalialorenacardoso@gmail.com>
2022-02-06 13:31:38 -0300

    Acrescentando comentários
    Co-Authored-By: Francisco Heronildo <heronildo.2010@hotmail.com>

--- src/caixeiroViajante.cpp
+++ src/caixeiroViajante.cpp
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define V 5 
+#define V 5 // define o número de vértices para o grafo
 
-
+//FUNÇÃO QUE IMPLEMENTA O PROBLEMA DO CAIXEIRO VIAJANTE
 int caixeiroViajante(int graph[][V], int s) {
 
+    
+    //armazena todos os vértices além do vértice de origem no vetor vertice
     vector<int> vertice;
     for (int i = 0; i < V; i++)
         if(i!=s)
             vertice.push_back(i);
 
+    //armazena o peso mínimo no ciclo Hamiltoniano
     int min_Rota = INT_MAX;
     do {
 
+        //armazena o peso da rota atual (custo)
         int atual_pesoRota = 0;
         int k=s;
+
+        //calcula o peso da rota atual
         for (int i = 0; i < vertice.size(); i++) {
             atual_pesoRota += graph[k][vertice[i]];
             k = vertice[i];
         }
         atual_pesoRota += graph[k][s];
 
+        //atualiza o mínimo
         min_Rota = min(min_Rota, atual_pesoRota);
 
     } while (next_permutation(vertice.begin(), vertice.end()));
@@ -31,6 +38,7 @@ int caixeiroViajante(int graph[][V], int s) {
 
 int main () {
 
+    // matriz representando o grafo
     int graph[][V] = {{ 0, 2, 3, 2, 3 },
                       { 2, 0, 3, 4, 1 },
                       { 3, 3, 0, 2, 4 },
